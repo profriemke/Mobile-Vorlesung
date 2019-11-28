35d3f60f239b45ba3ce71146ccb817b271279dbb
Iemke <riemke-gurzki@hdm-stuttgart.de>
2019-11-28 20:20:56 +0100

    Behebung kleinerer Fehler und Package Updates

--- mobile2_2_hdmphone_list/App.js
+++ mobile2_2_hdmphone_list/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 
 export default class App extends Component {
   state={
